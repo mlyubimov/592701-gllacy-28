32c36dabc2405a6a9e3c924aba071d904ebfb6ae
mlyubimov <mlyubimov.web@gmail.com>
2020-06-02 20:49:42 +0300

    Completed the layout of the site
    Fully written stylization. Added animation. The site is written in flex and grid format. The site is adapted to any volume of text in blocks. The script is written

---
+++ js/all.js
@@ -0,0 +1,50 @@
+document.getElementById('popup').addEventListener('click', function (e) {
+    e.preventDefault();
+    document.getElementById('feedback-popup').classList.add('feedback-brightness-on');
+    document.body.classList.add('hidden-overflow');
+}, false);
+
+document.getElementById('close-popup').addEventListener('click', function () {
+    document.getElementById('feedback-popup').classList.remove('feedback-brightness-on');
+    document.body.classList.remove('hidden-overflow');
+});
+
+function validateForm() {
+    let name = document.getElementById('user-name');
+    let email = document.getElementById('feedback-email');
+    let text = document.getElementById('feedback-text');
+    if (!name.checkValidity()) {
+        document.getElementById('feedback-section').classList.add('feedback-alert');
+        setTimeout(function () {
+            document.getElementById('feedback-section').classList.remove('feedback-alert');
+        },500);
+    } else if (!email.checkValidity()) {
+        document.getElementById('feedback-section').classList.add('feedback-alert');
+        setTimeout(function () {
+            document.getElementById('feedback-section').classList.remove('feedback-alert');
+        },500);
+    } else if (!text.checkValidity()) {
+        document.getElementById('feedback-section').classList.add('feedback-alert');
+        setTimeout(function () {
+            document.getElementById('feedback-section').classList.remove('feedback-alert');
+        },500);
+    }
+}
+
+let catalog = document.getElementById('site-navigation__catalog');
+
+document.getElementById('site-navigation__item').addEventListener('mouseover', function () {
+    catalog.classList.add('catalog-hover');
+});
+document.getElementById('site-navigation__item').addEventListener('mouseout', function () {
+    catalog.classList.remove('catalog-hover');
+});
+
+document.querySelectorAll('.dropdown-menu a').forEach(function (e) {
+    e.addEventListener('focus', function () {
+        catalog.classList.add('catalog-hover');
+    });
+    e.addEventListener('blur', function () {
+        catalog.classList.remove('catalog-hover');
+    });
+});
